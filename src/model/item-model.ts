39b011d3a1101aba3492ce10c007d058fe97753b
unknown <rijal.1344@gmail.com>
2025-01-28 10:53:32 +0700

    feat: add item

--- src/model/item-model.ts
+++ src/model/item-model.ts
@@ -9,6 +9,12 @@ export interface ItemResponse {
   updated_at: Date;
 }
 
+export interface ItemRequest {
+  code: string;
+  name: string;
+  price: number;
+}
+
 export interface Item {
   id: number;
   kode: string;
@@ -27,4 +33,15 @@ export function toItemsResponse(items: m_barang[]): ItemResponse[] {
     created_at: item.created_at,
     updated_at: item.updated_at,
   }));
+}
+
+export function toItemResponse(item: m_barang): ItemResponse {
+  return {
+    id: item.id,
+    kode: item.kode,
+    nama: item.nama,
+    harga: item.harga,
+    created_at: item.created_at,
+    updated_at: item.updated_at,
+  };
 }
